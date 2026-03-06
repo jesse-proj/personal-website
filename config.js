1a910559b4f46c97fba5fcde73e9d58893d1d278
jesse-proj <jian.santiago39@gmail.com>
2026-03-06 16:58:21 +0800

    Fetched data from back-end and fixed view for 4k screens.

---
+++ config.js
@@ -0,0 +1,4 @@
+window.RECENT_BLOGS_NUM = 5;
+
+window.LATEST_STATUS_URL = 'http:localhost:5000/admin/get-latest-status/Faf';
+window.RECENT_BLOGS_URL = 'http:localhost:5000/blog/get-recent-blogs/' + window.RECENT_BLOGS_NUM;
